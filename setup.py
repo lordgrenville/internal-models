30995d308bc1bc510fc2a12b6cd305963bf778f0
Josh Friedlander <16547083+lordgrenville@users.noreply.github.com>
2021-05-10 23:41:19 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='internal-models',
+      version='0.0.2',
+      description='Models package',
+      packages=["models"],
+      zip_safe=False,
+      install_requires=[])
